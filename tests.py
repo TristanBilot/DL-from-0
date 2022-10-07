412293be37415ca5d7bbad048c4f3a73de2d5fce
Tristan Bilot <bilot.tristan@hotmail.fr>
2022-10-07 17:33:12 +0200

    ✅ add tests for small MLP cases

--- tests.py
+++ tests.py
@@ -132,3 +132,86 @@ def test_mse_1():
     dy_hat_w_torch = y_hat_torch.grad.cpu().detach().numpy()
 
     assert np.all(y_hat.gradient == dy_hat_w_torch)
+
+
+def test_1_layer_mse():
+    """Tests a basic MLP with one hidden layer and MSE loss
+    """
+    a = np.array([[-0.5,  0.5], [-2,  2]])
+    b = np.array([[1.2,  1.2], [1.2,  1.2]])
+    c = np.array([[-2,  2], [1.5, 1.5]])
+    d = np.array([[1., 1.], [1., 1.]])
+
+    # our
+    W1 = ad.Tensor(a)
+    b1 = ad.Tensor(b)
+    X = ad.Tensor(c)
+    y = ad.Tensor(d)
+
+    sig = ad.Tensor().sigmoid
+    mse = ad.Tensor().mse_loss
+
+    loss = mse(sig(W1 @ X + b1), y)
+    loss.backpropagate()
+
+    # torch
+    W1_torch = torch.tensor(a, requires_grad=True)
+    b1_torch = torch.tensor(b, requires_grad=True)
+    X_torch = torch.tensor(c, requires_grad=True)
+    y_torch = torch.tensor(d, requires_grad=True)
+
+    sig_func = torch.nn.Sigmoid()
+    mse_func = torch.nn.MSELoss()
+
+    loss_torch = mse_func(sig_func(W1_torch @ X_torch + b1_torch), y_torch)
+    loss_torch.backward()
+
+    dy_hat_W1_torch = W1_torch.grad.cpu().detach().numpy()
+
+    assert np.all(W1.gradient == dy_hat_W1_torch)
+
+
+def test_2_layers_mse():
+    """Tests a basic MLP with two hidden layers and MSE loss
+    """
+    a = np.array([[-0.5,  0.5], [-2,  2]])
+    b = np.array([[1.2,  1.2], [1.2,  1.2]])
+    c = np.array([[-2,  2], [1.5, 1.5]])
+    d = np.array([[1., 1.], [1., 1.]])
+
+    e = np.array([[-1.5,  1.5], [-2,  2]])
+    f = np.array([[0.6,  0.7], [1.2,  1.2]])
+
+    # our
+    W1 = ad.Tensor(a)
+    b1 = ad.Tensor(b)
+    W2 = ad.Tensor(e)
+    b2 = ad.Tensor(f)
+    X = ad.Tensor(c)
+    y = ad.Tensor(d)
+
+    sig = ad.Tensor().sigmoid
+    mse = ad.Tensor().mse_loss
+
+    layer1 = sig(W1 @ X + b1)
+    loss = mse(sig(W2 @ layer1 + b2), y)
+    loss.backpropagate()
+
+    # torch
+    W1_torch = torch.tensor(a, requires_grad=True)
+    b1_torch = torch.tensor(b, requires_grad=True)
+    W2_torch = torch.tensor(e, requires_grad=True)
+    b2_torch = torch.tensor(f, requires_grad=True)
+    X_torch = torch.tensor(c, requires_grad=True)
+    y_torch = torch.tensor(d, requires_grad=True)
+
+    sig_func = torch.nn.Sigmoid()
+    mse_func = torch.nn.MSELoss()
+
+    layer1_torch = sig_func(W1_torch @ X_torch + b1_torch)
+    loss_torch = mse_func(sig_func(W2_torch @ layer1_torch + b2_torch), y_torch)
+    loss_torch.backward()
+
+    dy_hat_W2_torch = W2_torch.grad.cpu().detach().numpy()
+
+    assert np.all(np.round(W2.gradient, 4) == np.round(dy_hat_W2_torch, 4))
