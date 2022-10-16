43191168fbbb18458913fdeb51a5e3ee698b38c3
Tristan Bilot <bilot.tristan@hotmail.fr>
2022-10-16 22:00:47 +0200

    ✨ plots for course 4

--- 4-xor-sinus/plot_figures.py
+++ 4-xor-sinus/plot_figures.py
@@ -1,12 +1,12 @@
-import math
+import matplotlib.pyplot as plt
 import numpy as np
-import torch
 
 import autodiff as ad
 import visualization as viz
 from layers import Linear, MSE, Sigmoid, ReLU
 from models import MLP
 from optimizers import SGD, Adam
+from utils import generate_sin_dataset, shuffle_dataset
 
 
 def plot_xor_layers_loss_acc():
@@ -58,4 +58,69 @@ def plot_xor_layers_loss_acc():
     viz.plot_lists(losses_)
     viz.plot_lists(accuracies_)
 
-plot_xor_layers_loss_acc()
+
+def plot_sin_layers_loss_acc():
+    accuracies_, losses_, test_accuracies_ = [], [], []
+
+    for i in range(6):
+        X_train, X_test, y_train, y_test = generate_sin_dataset(200)
+
+        model = MLP(
+            Linear(1, 500),
+            ReLU(),
+            Linear(500, 1),
+        )
+
+        lr=0.0008 - i * 0.00002
+        optimizer = Adam(params=model.params, lr=lr)
+        loss_fn = MSE()
+
+        epochs = 10000
+        losses, accuracies, test_accuracies = [], [], []
+
+        def test(model):
+            y_hat = model(X_test)
+            sum = ad.Tensor().sum
+            correct_preds = sum(y_hat.near_eq(y_test, round=1)).val
+            accuracy = correct_preds / y_test.shape[0]
+            
+            return accuracy
+
+        def plot(model):
+            y_hat = model(X_test)
+
+            plt.scatter(X_test.val, y_hat.val, color='red', label="Predictions")
+            plt.scatter(X_test.val, y_test.val, color='green', label="Truth")            
+            
+            plt.legend(loc="upper right")
+            plt.show()
+
+        for i in range(epochs):
+            X_train, X_test, y_train, y_test = shuffle_dataset(X_train, X_test, y_train, y_test)
+
+            optimizer.zero_grad()
+
+            loss, y_hat = model.train(X=X_train, y=y_train, optimizer=optimizer, loss_fn=loss_fn)
+            losses.append(loss.val)
+
+            sum = ad.Tensor().sum
+            correct_preds = sum(y_hat.near_eq(y_train, round=1)).val
+            accuracy = correct_preds / y_train.shape[0]
+            
+            test_acc = test(model)
+            accuracies.append(accuracy)
+            test_accuracies.append(test_acc)
+
+            if i % 200 == 0:
+                print(test_acc, accuracy)
+
+        # plot(model)
+        accuracies_.append((accuracies, f'lr={round(lr, 5)}'))
+        losses_.append((losses, f'lr={round(lr, 5)}'))
+        test_accuracies_.append((test_accuracies, f'lr={round(lr, 5)}'))
+
+    viz.plot_lists(losses_)
+    viz.plot_lists(accuracies_)
+    viz.plot_lists(test_accuracies_)
+
+plot_sin_layers_loss_acc()
---
+++ 4-xor-sinus/utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+import autodiff as ad
+
+
+def generate_sin_data(n: int):
+    dataset = np.empty([n, 2])
+    data = 2 * np.pi * np.random.random_sample((n))
+    dataset[:,0] = data
+    dataset[:,1] = np.sin(data)
+    X = dataset[:,0]
+    y =  dataset[:,1]
+    
+    return X, y
+
+
+def generate_sin_dataset(n: int):
+    X, y = generate_sin_data(n)
+    X_train, X_test = \
+        ad.Tensor(X[:int(len(X) * 0.5)].reshape(-1, 1)), \
+        ad.Tensor(X[int(len(X) * 0.5):].reshape(-1, 1))
+    y_train, y_test = \
+        ad.Tensor(y[:int(len(y) * 0.5)].reshape(-1, 1)), \
+        ad.Tensor(y[int(len(y) * 0.5):].reshape(-1, 1))
+    
+    return X_train, X_test, y_train, y_test
+
+
+def shuffle_dataset(
+    X_train: ad.Tensor,
+    X_test: ad.Tensor,
+    y_train: ad.Tensor,
+    y_test: ad.Tensor,
+):
+    indices = np.arange(X_train.shape[0])
+    np.random.shuffle(indices)
+    X_train = X_train.val[indices]
+    y_train = y_train.val[indices]
+
+    indices = np.arange(X_test.shape[0])
+    np.random.shuffle(indices)
+    X_test = X_test.val[indices]
+    y_test = y_test.val[indices]
+
+    X_train, X_test = ad.Tensor(X_train), ad.Tensor(X_test)
+    y_train, y_test = ad.Tensor(y_train), ad.Tensor(y_test)
+
+    return X_train, X_test, y_train, y_test
